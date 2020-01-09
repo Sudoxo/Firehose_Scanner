eab780d375f3a24a7d158b28b425af64e305acf2
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-01-09 21:39:14 +0100

    Add separate tables for different actions

--- main.py
+++ main.py
@@ -16,20 +16,45 @@ def parse(c, conn):
         act = e['type']
         wh = e['who']
         t = int_to_time(int(e['ts']))
+        
         if(act == "post" or act == "new"):
             c.execute('SELECT count(*) FROM articles WHERE link = ? AND creation_time = ? AND who = ?',[lin,t,wh])
             if(c.fetchone()[0] == 0):
                 print("Inserted: " + tit)
                 c.execute('INSERT INTO articles VALUES (?,?,?,?)', [lin, tit, wh, t])
                 conn.commit()
+                
         else:
             c.execute('SELECT count(*) FROM articles WHERE link = ?', [lin])
             if(c.fetchone()[0] == 1):
-                c.execute('SELECT count(*) FROM actions WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
-                if(c.fetchone()[0] == 0):
-                    print("Inserted action: " + act + " on title: " + tit)
-                    c.execute('INSERT INTO actions VALUES (?,?,?,?)', [lin, act, wh, t])
-                    conn.commit()
+                #VOTES
+                if(act == "vote"):
+                    c.execute('SELECT count(*) FROM votes WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
+                    if(c.fetchone()[0] == 0):
+                        print("Inserted action: " + act + " on title: " + tit)
+                        c.execute('INSERT INTO votes VALUES (?,?,?)', [lin, wh, t])
+                        conn.commit()
+                #PROBLEMS
+                elif(act == "problem"):
+                    c.execute('SELECT count(*) FROM problems WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
+                    if(c.fetchone()[0] == 0):
+                        print("Inserted action: " + act + " on title: " + tit)
+                        c.execute('INSERT INTO problems VALUES (?,?,?)', [lin, wh, t])
+                        conn.commit()
+                #COMMENTS
+                elif(act == "comment"):
+                    c.execute('SELECT count(*) FROM comments WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
+                    if(c.fetchone()[0] == 0):
+                        print("Inserted action: " + act + " on title: " + tit)
+                        c.execute('INSERT INTO comments VALUES (?,?,?)', [lin, wh, t])
+                        conn.commit()
+                #EDITS
+                elif(act == "cedited"):
+                    c.execute('SELECT count(*) FROM edits WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
+                    if(c.fetchone()[0] == 0):
+                        print("Inserted action: " + act + " on title: " + tit)
+                        c.execute('INSERT INTO edits VALUES (?,?,?)', [lin, wh, t])
+                        conn.commit()
 def main():
     x = path.exists('articles.db')
     conn = sqlite3.connect('articles.db')
@@ -38,8 +63,14 @@ def main():
     if(not x):
         c.execute('''CREATE TABLE articles
         (link varchar, title varchar, who varchar, creation_time time)''')
-        c.execute('''CREATE TABLE actions
-        (link varchar, action varchar, who varchar, action_time time)''')
+        c.execute('''CREATE TABLE votes
+        (link varchar, who varchar, action_time time)''')
+        c.execute('''CREATE TABLE problems
+        (link varchar, who varchar, action_time time)''')
+        c.execute('''CREATE TABLE comments
+        (link varchar, who varchar, action_time time)''')
+        c.execute('''CREATE TABLE edits
+        (link varchar, who varchar, action_time time)''')
         conn.commit()
     while(True):
         parse(c, conn)
--- read_db.py
+++ read_db.py
@@ -4,10 +4,25 @@ import sqlite3
 def read_db():
     conn = sqlite3.connect('articles.db')
     c = conn.cursor()
+    print("#################")
+    print("ARTICLES:")
     for row in c.execute('SELECT * FROM articles'):
         print(row)
     print("#################")
-    for row in c.execute('SELECT * FROM actions'):
+    print("VOTES:")
+    for row in c.execute('SELECT * FROM votes'):
+        print(row)
+    print("#################")
+    print("PROBLEMS:")
+    for row in c.execute('SELECT * FROM problems'):
+        print(row)
+    print("#################")
+    print("COMMENTS:")
+    for row in c.execute('SELECT * FROM comments'):
+        print(row)
+    print("#################")
+    print("EDITS:")
+    for row in c.execute('SELECT * FROM edits'):
         print(row)
     conn.close()
 
