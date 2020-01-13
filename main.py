699ee64870d8eb7387cba1cd5aa69a776e8504d5
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-01-13 01:16:23 +0100

    Add reverse iteration, optimalization, constraints
    The program is not longer getting the same data many times. I've added constraints  to tables.

--- main.py
+++ main.py
@@ -6,55 +6,60 @@ from os import path
 
 def int_to_time(x):
     return time.strftime('%H:%M:%S', time.gmtime(x+3600))
-
+last = [0,"","",""]
 def parse(c, conn):
+    global last
     with urllib.request.urlopen("https://www.meneame.net/backend/sneaker2") as url:
         data = json.loads(url.read().decode())
-    for e in data['events']:
+    flag = 0
+    for e in reversed(data['events']):
         lin = e['link']
         tit = e['title']
         act = e['type']
         wh = e['who']
         t = int_to_time(int(e['ts']))
-        
+        if(flag == 0):
+            if(int(e['ts']) < last[0]):
+                continue
+            if(int(e['ts']) == last[0]):
+                if(wh == last[1] and act == last[2] and lin == last[3]):
+                    flag = 1
+                continue
+        flag = 1
+        ############print(str(t) + "    " + tit)
         if(act == "post" or act == "new"):
-            c.execute('SELECT count(*) FROM articles WHERE link = ? AND creation_time = ? AND who = ?',[lin,t,wh])
-            if(c.fetchone()[0] == 0):
-                print("Inserted: " + tit)
-                c.execute('INSERT INTO articles VALUES (?,?,?,?)', [lin, tit, wh, t])
-                conn.commit()
+            print("Inserted: " + tit)
+            c.execute('INSERT INTO articles VALUES (?,?,?,?)', [tit, lin, wh, t])
+            conn.commit()
                 
         else:
-            c.execute('SELECT count(*) FROM articles WHERE link = ?', [lin])
+            c.execute('SELECT count(*) FROM articles WHERE title = ?', [tit])
             if(c.fetchone()[0] == 1):
                 #VOTES
                 if(act == "vote"):
-                    c.execute('SELECT count(*) FROM votes WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
-                    if(c.fetchone()[0] == 0):
-                        print("Inserted action: " + act + " on title: " + tit)
-                        c.execute('INSERT INTO votes VALUES (?,?,?)', [lin, wh, t])
-                        conn.commit()
+                    print("Inserted action: " + act + " on title: " + tit)
+                    c.execute('INSERT INTO votes VALUES (?,?,?)', [tit, wh, t])
+                    conn.commit()
                 #PROBLEMS
                 elif(act == "problem"):
-                    c.execute('SELECT count(*) FROM problems WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
-                    if(c.fetchone()[0] == 0):
-                        print("Inserted action: " + act + " on title: " + tit)
-                        c.execute('INSERT INTO problems VALUES (?,?,?)', [lin, wh, t])
-                        conn.commit()
+                    print("Inserted action: " + act + " on title: " + tit)
+                    c.execute('INSERT INTO problems VALUES (?,?,?)', [tit, wh, t])
+                    conn.commit()
                 #COMMENTS
                 elif(act == "comment"):
-                    c.execute('SELECT count(*) FROM comments WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
-                    if(c.fetchone()[0] == 0):
-                        print("Inserted action: " + act + " on title: " + tit)
-                        c.execute('INSERT INTO comments VALUES (?,?,?)', [lin, wh, t])
-                        conn.commit()
+                    print("Inserted action: " + act + " on title: " + tit)
+                    c.execute('INSERT INTO comments VALUES (?,?,?)', [tit, wh, t])
+                    conn.commit()
                 #EDITS
                 elif(act == "cedited"):
-                    c.execute('SELECT count(*) FROM edits WHERE link = ? AND action_time = ? AND who = ?',[lin,t,wh])
-                    if(c.fetchone()[0] == 0):
-                        print("Inserted action: " + act + " on title: " + tit)
-                        c.execute('INSERT INTO edits VALUES (?,?,?)', [lin, wh, t])
-                        conn.commit()
+                    print("Inserted action: " + act + " on title: " + tit)
+                    c.execute('INSERT INTO edits VALUES (?,?,?)', [tit, wh, t])
+                    conn.commit()
+
+    last[0] = int(data['events'][0]['ts'])
+    last[1] = data['events'][0]['who']
+    last[2] = data['events'][0]['type']
+    last[3] = data['events'][0]['link']
 def main():
     x = path.exists('articles.db')
     conn = sqlite3.connect('articles.db')
@@ -62,15 +67,15 @@ def main():
     #Create tables
     if(not x):
         c.execute('''CREATE TABLE articles
-        (link varchar, title varchar, who varchar, creation_time time)''')
+        (title varchar PRIMARY KEY, link varchar, who varchar, creation_time time)''')
         c.execute('''CREATE TABLE votes
-        (link varchar, who varchar, action_time time)''')
+        (title varchar, who varchar, action_time time, FOREIGN KEY (title) REFERENCES articles (title))''')
         c.execute('''CREATE TABLE problems
-        (link varchar, who varchar, action_time time)''')
+        (title varchar, who varchar, action_time time, FOREIGN KEY (title) REFERENCES articles (title))''')
         c.execute('''CREATE TABLE comments
-        (link varchar, who varchar, action_time time)''')
+        (title varchar, who varchar, action_time time, FOREIGN KEY (title) REFERENCES articles (title))''')
         c.execute('''CREATE TABLE edits
-        (link varchar, who varchar, action_time time)''')
+        (title varchar, who varchar, action_time time, FOREIGN KEY (title) REFERENCES articles (title))''')
         conn.commit()
     while(True):
         parse(c, conn)
